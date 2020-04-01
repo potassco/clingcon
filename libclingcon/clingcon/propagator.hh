b471db06e72562cad37b85fc8fb454d826b989cc
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-04-01 22:54:39 +0200

    fine-tuning

--- libclingcon/clingcon/propagator.hh
+++ libclingcon/clingcon/propagator.hh
@@ -157,7 +157,7 @@ private:
     }
 
     //! Add collected statistics in stats to the clingo's statistics.
-    static void add_statistics_(Clingo::UserStatistics &stats_map, Statistics &stats);
+    static void add_statistics_(Clingo::UserStatistics &root, Statistics &stats);
 
     //! Add a constraint to the program that has already been added to the
     //! master solver.
