eb1fe87a62d6bcdbd0a0af2181fca0459bfc2536
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-04-11 02:58:11 +0200

    remove unnecessary test case

--- libclingcon/tests/csp.cc
+++ libclingcon/tests/csp.cc
@@ -29,10 +29,6 @@
 
 using namespace Clingcon;
 
-TEST_CASE("fail") {
-    REQUIRE(solve("&minimize { x }. &sum{ x } >= 0 :- a. {a}.", -3, 3) == S({"x=-3"}));
-}
-
 TEST_CASE("solving", "[solving]") { // NOLINT
     SECTION("simple") {
         REQUIRE(solve("&sum{ x } > 0. &sum{ x } < 3. &sum { x } = y.") == S({
