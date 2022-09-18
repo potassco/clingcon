65bf93fa9a195ebe5623295aec04e01687df84d4
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-09-18 13:22:13 +0200

    fine-tuning

--- libpyclingcon/clingcon/__init__.py
+++ libpyclingcon/clingcon/__init__.py
@@ -1,8 +1,16 @@
+"""
+This module provides the clingcon theory.
+"""
+
 from clingo.theory import Theory
 from ._clingcon import lib as _lib, ffi as _ffi
 
 __all__ = ['ClingconTheory']
 
+
 class ClingconTheory(Theory):
+    """
+    A theory to register a propagator for linear constraints.
+    """
     def __init__(self):
         super().__init__("clingcon", _lib, _ffi)
