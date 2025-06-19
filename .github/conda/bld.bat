0cc341b625ab764ba5ac44dba8bef1fbd459e89c
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-06-19 23:36:06 +0200

    add conda workflow

---
+++ .github/conda/bld.bat
@@ -0,0 +1,12 @@
+cmake -S . -B build -G "Visual Studio 17 2022" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="lib" ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    -DPython_EXECUTABLE="%PYTHON%" ^
+    -DCLINGCON_MANAGE_RPATH=OFF ^
+    -DCLINGCON_BUILD_TESTS=ON ^
+    -DPYCLINGCON_INSTALL_DIR="%SP_DIR%"
+
+cmake --build build --config Release
+ctest --test-dir build -C Release
+cmake --build build --target install --config Release
---
+++ .github/conda/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+if [ -z "${PYTHON}" ]; then
+    PYTHON="$(which python)"
+fi
+
+cmake -S . -B build -G Ninja \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_LIBDIR="lib" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPython_ROOT_DIR="${PREFIX}" \
+    -DPython_EXECUTABLE="${PYTHON}" \
+    -DCLINGCON_MANAGE_RPATH=OFF \
+    -DCLINGCON_BUILD_TESTS=ON \
+    -DPYCLINGCON_INSTALL_DIR="${SP_DIR}"
+
+cmake --build build
+ctest --test-dir build
+cmake --build build --target install
