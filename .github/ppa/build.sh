2b924b4285c290976d6ea1d2369acc7b201588ef
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-11-28 17:07:13 +0100

    add action to deploy to cloudsmith

--- .github/ppa/build.sh
+++ .github/ppa/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function usage {
-    echo "./$(basename "$0") --build-number <n> {wip-20} {noble} {create,sync,changes,build,put,clean}*"
+    echo "./$(basename "$0") --type={ppa,cloudsmith} --build-number <n> {stable,wip-20} {noble,trixie} {create,sync,changes,build,put,clean}*"
 }
 
 if [[ $# -lt 1 ]]; then
@@ -10,8 +10,9 @@ if [[ $# -lt 1 ]]; then
 fi
 
 BUILD_NUMBER=""
+BUILD_TYPE="ppa"
 
-if ! ARGS=$(getopt -o '' --long build-number: -- "$@"); then
+if ! ARGS=$(getopt -o '' --long build-number:,type: -- "$@"); then
     usage
     exit 1
 fi
@@ -21,6 +22,10 @@ while true; do
     case "$1" in
     --build-number)
         BUILD_NUMBER="$2"
+        shift 2
+        ;;
+    --type)
+        BUILD_TYPE="$2"
         shift 2
         ;;
     --)
@@ -34,13 +39,23 @@ while true; do
     esac
 done
 
-set -e
+set -ex
 
 ref="${1}"
 shift
 
+case "${BUILD_TYPE}" in
+ppa) ;;
+cloudsmith) ;;
+*)
+    usage
+    exit 1
+    ;;
+esac
+
 case "${ref}" in
 wip-20) ;;
+stable) ;;
 *)
     usage
     exit 1
@@ -50,8 +65,14 @@ esac
 rep="${1}"
 shift
 
+DISTRIBUTION=ubuntu
+PBUILDER_ARGS=()
 case "${rep}" in
 noble) ;;
+trixie)
+    PBUILDER_ARGS+=(--mirror http://deb.debian.org/debian)
+    DISTRIBUTION=debian
+    ;;
 *)
     usage
     exit 1
@@ -62,13 +83,23 @@ for act in "${@}"; do
     echo "${act}"
     case "${act}" in
     _ppa)
-        apt-get install -y software-properties-common
-        add-apt-repository -y "ppa:potassco/${ref}"
+        if [[ "${DISTRIBUTION}" == ubuntu ]]; then
+            apt-get install -y software-properties-common
+            add-apt-repository -y "ppa:potassco/${ref}"
+        else
+            apt-get install -y curl ca-certificates
+            curl -1sLf "https://dl.cloudsmith.io/public/potassco/${ref}/setup.deb.sh" | bash
+        fi
         apt-get update
         apt-get install -y tree debhelper
         ;;
     create)
-        sudo pbuilder create --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --distribution "${rep}" --debootstrapopts --variant=buildd
+        sudo apt-get -y update
+        sudo apt-get -y install pbuilder pbuilder-scripts debootstrap devscripts dh-make dput dh-python
+        if [[ "${DISTRIBUTION}" == debian ]]; then
+            sudo apt-get -y install debian-archive-keyring
+        fi
+        sudo pbuilder create --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --distribution "${rep}" "${PBUILDER_ARGS[@]}" --debootstrapopts --variant=buildd
         sudo pbuilder execute --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --save-after-exec -- build.sh "${ref}" "${rep}" _ppa
         ;;
     sync)
@@ -95,8 +126,13 @@ for act in "${@}"; do
             echo "No build number given"
             exit 1
         fi
+        if [[ "$DISTRIBUTION" == "ubuntu" ]]; then
+            SUFFIX="-${rep}${BUILD_NUMBER}"
+        else
+            SUFFIX=".${BUILD_NUMBER}"
+        fi
         cat >"${rep}/debian/changelog" <<EOF
-clingcon (${VERSION}-${rep}${BUILD_NUMBER}) ${rep}; urgency=medium
+clingcon (${VERSION}${SUFFIX}) ${rep}; urgency=medium
 
   * build for git revision $(git rev-parse HEAD)
 
@@ -104,13 +140,20 @@ clingcon (${VERSION}-${rep}${BUILD_NUMBER}) ${rep}; urgency=medium
 EOF
         ;;
     build)
-        VERSION="$(head -n 1 "${rep}/debian/changelog" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\(-[a-z0-9]\+\)\?')"
-        (
-            cd "${rep}"
-            pdebuild --buildresult .. --auto-debsign --debsign-k 744d959e10f5ad73f9cf17cc1d150536980033d5 -- --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --source-only-changes
-            sed -i '/\.buildinfo$/d' "../clingcon_${VERSION}_source.changes"
-            debsign --re-sign -k744d959e10f5ad73f9cf17cc1d150536980033d5 "../clingcon_${VERSION}_source.changes"
-        )
+        if [[ "${BUILD_TYPE}" == "ppa" ]]; then
+            VERSION="$(head -n 1 "${rep}/debian/changelog" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\(-[a-z0-9]\+\)\?')"
+            (
+                cd "${rep}"
+                pdebuild --buildresult .. --auto-debsign --debsign-k 744d959e10f5ad73f9cf17cc1d150536980033d5 -- --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --source-only-changes
+                sed -i '/\.buildinfo$/d' "../clingcon_${VERSION}_source.changes"
+                debsign --re-sign -k744d959e10f5ad73f9cf17cc1d150536980033d5 "../clingcon_${VERSION}_source.changes"
+            )
+        else
+            (
+                cd "${rep}"
+                pdebuild --buildresult .. -- --basetgz "/var/cache/pbuilder/${ref}-${rep}.tgz" --source-only-changes
+            )
+        fi
         ;;
     put)
         VERSION="$(head -n 1 "${rep}/debian/changelog" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\(-[a-z0-9]\+\)\?')"
@@ -135,6 +178,7 @@ EOF
             "${rep}"/debian/debhelper-build-stamp \
             "${rep}"/debian/tmp \
             "${rep}"/obj-x86_64-linux-gnu \
+            ./*.md \
             ./*.build \
             ./*.deb \
             ./*.dsc \
--- .github/trigger.py
+++ .github/trigger.py
@@ -15,6 +15,7 @@ TOKEN_FILE = os.path.expanduser("~/.tokens")
 WORKFLOW_ID_CONDA = "169621704"
 WORKFLOW_ID_PYPI = "190650163"
 WORKFLOW_ID_PPA = "190670412"
+WORKFLOW_ID_CLOUDSMITH = "211223736"
 
 
 def get_token():
@@ -107,6 +108,11 @@ def main():
             args.branch,
             {"build_number": args.build_number, "type": "stable"},
         )
+        dispatch_workflow(
+            WORKFLOW_ID_CLOUDSMITH,
+            args.branch,
+            {"build_number": args.build_number, "type": "stable"},
+        )
     elif args.command == "dev":
         dispatch_workflow(
             WORKFLOW_ID_CONDA,
@@ -120,6 +126,11 @@ def main():
         )
         dispatch_workflow(
             WORKFLOW_ID_PPA,
+            args.branch,
+            {"build_number": "auto", "type": "wip-20"},
+        )
+        dispatch_workflow(
+            WORKFLOW_ID_CLOUDSMITH,
             args.branch,
             {"build_number": "auto", "type": "wip-20"},
         )
